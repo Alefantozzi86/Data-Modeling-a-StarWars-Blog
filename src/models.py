6eac5b766fe3c9d01622d913d4d9d6aedb083964
Alefantozzi86 <alefantozzi86@gmail.com>
2023-06-05 11:58:56 +0000

    proyecto

--- src/models.py
+++ src/models.py
@@ -7,23 +7,60 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(50), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(15), nullable=False)
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Data(Base):
+    __tablename__ = 'data'   
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50), nullable=False, unique=True)
+   
+class Favorite(Base):
+    __tablename__ = 'favorite'   
+    id= Column(Integer, primary_key=True)
+    name =  Column(String(50), nullable=False, unique=True)
+    category = Column(String(50), nullable=False, unique=True)
+    data_id = Column(Integer, ForeignKey('data.id'))
+    user_id  = Column(Integer, ForeignKey('user.id'))
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name_character = Column(String(50), nullable=False, unique=True)
+    gender = Column(String(10))
+    height = Column(Integer)
+    hair_color = Column(String(50))
+    skin_color = Column(String(50))
+    eye_color = Column(String(50))
+    data_id  = Column(Integer, ForeignKey('data.id'))
+
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name_planets = Column(String(50), nullable=False, unique=True)
+    climate = Column(String(50))
+    population = Column(Integer)
+    orbital_period = Column(Integer)
+    rotation_period = Column(Integer)
+    diameter = Column(Integer)
+    data_id  = Column(Integer, ForeignKey('data.id'))
+
+class Films(Base):
+    __tablename__ = 'films'
+    id =  Column(Integer, primary_key=True)
+    title = Column(String(50))
+    episode_id = Column(String(50))
+    director = Column(String(50))
+    producer = Column(String(50))
+    release_date = Column(Integer)
+    data_id  = Column(Integer, ForeignKey('data.id'))
+
 
     def to_dict(self):
         return {}
